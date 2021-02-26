366aa55508d35ac235425d6597bd57771e9fab9a
Sergey Kornienko <skornienko29@gmail.com>
2021-02-26 15:45:39 +0300

    add test and fixtures step 2

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,5 +1,6 @@
 from os import path
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 import re
 import requests
 
@@ -8,12 +9,23 @@ def get_name(url):
     parsed = urlparse(url)
     scheme = "%s://" % parsed.scheme
     name = parsed.geturl().replace(scheme, '', 1)
-    return ''.join([re.sub(r'[^a-zA-Z0-9]', '-', name), '.html'])
+    return ''.join([re.sub(r'[^a-zA-Z0-9]', '-', name)])
 
 
 def download(url, path_file=''):
-    t = requests.request('GET', url).text
-    name = path.join(*(path_file, get_name(url)))
+    html_doc = requests.request('GET', url).text
+    # soup = BeautifulSoup(html_doc, 'html5lib')
+    # print(soup.prettify())
+    # for src in soup.find_all('img'):
+
+    name = path.join(*(path_file, ''.join([get_name(url), '.html'])))
     with open(name, 'w', encoding='utf-8') as infile:
-        infile.write(t)
+        infile.write(html_doc)
     return name
+
+
+if __name__ == '__main__':
+    download('https://ru.hexlet.io/courses')
+
+
+
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,12 +2,45 @@ import requests_mock
 from page_loader import download
 import tempfile
 from os import path
+import pytest
 
 
-def test_download_file():
+@pytest.mark.parametrize(
+    'path_input_file,path_expected_file,path_output_file,char,url',
+    [
+        ('tests/fixtures/index.html',
+         'tests/fixtures/download_index.html',
+         'ru-hexlet-io-courses.html',
+         'r',
+         'https://ru.hexlet.io/courses',
+         ),
+        ('tests/fixtures/nodejs.png',
+         'tests/fixtures/nodejs.png',
+         'ru-hexlet-io-courses_files/nodejs.png',
+         'rb',
+         'https://ru.hexlet.io/assets/professions/nodejs.png',
+         ),
+    ],
+)
+def test_download_file(
+        path_input_file,
+        path_expected_file,
+        path_output_file,
+        char,
+        url):
+    with open('tests/fixtures/index.html') as infile:
+        html_input = infile.read()
+    with open(path_input_file, char) as infile:
+        file_input = infile.read()
+    with open(path_expected_file, char) as infile:
+        file_output = infile.read()
     with tempfile.TemporaryDirectory() as direct:
         with requests_mock.Mocker() as mock:
-            mock.get('https://ru.hexlet.io/courses', text='data')
+            mock.get('https://ru.hexlet.io/courses', text=html_input)
+            if char == 'rb':
+                mock.get(url, content=file_input)
+            else:
+                mock.get(url, text=file_input)
             download('https://ru.hexlet.io/courses', path_file=direct)
-        with open(path.join(*(direct, 'ru-hexlet-io-courses.html'))) as infile:
-            assert infile.read() == 'data'
+            with open(path.join(*(direct, path_output_file)), char) as infile:
+                assert infile.read() == file_output
