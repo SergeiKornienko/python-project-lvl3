ca5333905f9ce2e4ffaee3d32d926eec428ab434
Sergey Kornienko <skornienko29@gmail.com>
2021-02-28 13:59:19 +0300

    add download img step 2

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,31 +1,70 @@
-from os import path
-from urllib.parse import urlparse
+from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup
 import re
 import requests
+import os
+from os.path import join
 
 
-def get_name(url):
-    parsed = urlparse(url)
-    scheme = "%s://" % parsed.scheme
-    name = parsed.geturl().replace(scheme, '', 1)
-    return ''.join([re.sub(r'[^a-zA-Z0-9]', '-', name)])
+def parse_url(url):
+    dict_url = {}
+    split_url = os.path.splitext(url)
+    dict_url['url'] = os.path.split(url)[0]
+    parsed_url = urlparse(split_url[0])
+    dict_url['suffix'] = split_url[1]
+    dict_url['netloc'] = re.sub(r'[^a-zA-Z0-9]', '-', parsed_url.netloc)
+    dict_url['path'] = re.sub(r'[^a-zA-Z0-9]', '-', parsed_url.path)
+    url_without_scheme_and_suffix = parsed_url.netloc + parsed_url.path
+    dict_url['url_without_scheme_and_suffix'] = url_without_scheme_and_suffix
+    dict_url['name_without_suffix'] = re.sub(
+        r'[^a-zA-Z0-9]',
+        '-',
+        url_without_scheme_and_suffix,
+    )
+    return dict_url
 
 
-def download(url, path_file=''):
-    html_doc = requests.request('GET', url).text
-    # soup = BeautifulSoup(html_doc, 'html5lib')
-    # print(soup.prettify())
-    # for src in soup.find_all('img'):
+def download(url, dir_for_save=''):
+    text_html = requests.request('GET', url).text
+    parsed_html_url = parse_url(url)
+    path_dir_for_files = join(
+        dir_for_save,
+        (parsed_html_url['name_without_suffix'] + '_files'),
+    )
+    os.mkdir(path_dir_for_files)
+    soup_from_html = BeautifulSoup(text_html, 'html5lib')
+    for img in soup_from_html.find_all('img', src=re.compile('^[^h]')):
+        parsed_img_url = parse_url(img['src'])
+        file_img = requests.request(
+            'GET',
+            urljoin(url, img['src']),
+        ).content
+        img['src'] = join(
+            (parsed_html_url['name_without_suffix'] + '_files'),
+            ''.join(
+                [parsed_html_url['netloc'],
+                 parsed_img_url['path'],
+                 parsed_img_url['suffix']],
+            ),
+        )
+        with open(
+                join(
+                    path_dir_for_files,
+                    ''.join(
+                        [parsed_html_url['netloc'],
+                         parsed_img_url['path'],
+                         parsed_img_url['suffix']],
+                    ),
+                ),
+                'wb') as infile:
+            infile.write(file_img)
+    with open(join(
+            dir_for_save,
+            (parsed_html_url['name_without_suffix'] + '.html'),
+    ), 'w') as infile:
+        infile.write(soup_from_html.decode(formatter="html5"))
+    return dir_for_save + parsed_html_url['name_without_suffix'] + '.html'
 
-    name = path.join(*(path_file, ''.join([get_name(url), '.html'])))
-    with open(name, 'w', encoding='utf-8') as infile:
-        infile.write(html_doc)
-    return name
 
-
-if __name__ == '__main__':
-    download('https://ru.hexlet.io/courses')
-
-
-
+# download('http://www.malero-guitare.fr/cours/gratte/')
+# download('https://www.crummy.com/software/BeautifulSoup/bs4/doc.ru/bs4ru.html')
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,45 +2,33 @@ import requests_mock
 from page_loader import download
 import tempfile
 from os import path
-import pytest
+from bs4 import BeautifulSoup
 
 
-@pytest.mark.parametrize(
-    'path_input_file,path_expected_file,path_output_file,char,url',
-    [
-        ('tests/fixtures/index.html',
-         'tests/fixtures/download_index.html',
-         'ru-hexlet-io-courses.html',
-         'r',
-         'https://ru.hexlet.io/courses',
-         ),
-        ('tests/fixtures/nodejs.png',
-         'tests/fixtures/nodejs.png',
-         'ru-hexlet-io-courses_files/nodejs.png',
-         'rb',
-         'https://ru.hexlet.io/assets/professions/nodejs.png',
-         ),
-    ],
-)
-def test_download_file(
-        path_input_file,
-        path_expected_file,
-        path_output_file,
-        char,
-        url):
+def test_download_file():
     with open('tests/fixtures/index.html') as infile:
         html_input = infile.read()
-    with open(path_input_file, char) as infile:
-        file_input = infile.read()
-    with open(path_expected_file, char) as infile:
-        file_output = infile.read()
+    with open('tests/fixtures/download_index.html') as infile:
+        html_expected = infile.read()
+    with open('tests/fixtures/nodejs.png', 'rb') as infile:
+        img = infile.read()
     with tempfile.TemporaryDirectory() as direct:
         with requests_mock.Mocker() as mock:
             mock.get('https://ru.hexlet.io/courses', text=html_input)
-            if char == 'rb':
-                mock.get(url, content=file_input)
-            else:
-                mock.get(url, text=file_input)
-            download('https://ru.hexlet.io/courses', path_file=direct)
-            with open(path.join(*(direct, path_output_file)), char) as infile:
-                assert infile.read() == file_output
+            mock.get(
+                'https://ru.hexlet.io/assets/professions/nodejs.png',
+                content=img,
+            )
+            download('https://ru.hexlet.io/courses', dir_for_save=direct)
+        with open(path.join(
+                direct,
+                'ru-hexlet-io-courses_files',
+                'ru-hexlet-io-assets-professions-nodejs.png',
+        ),
+                  'rb') as infile:
+            assert infile.read() == img
+        with open(path.join(*(direct, 'ru-hexlet-io-courses.html'))) as infile:
+            assert BeautifulSoup(
+                infile.read(),
+                'html5lib',
+            ) == BeautifulSoup(html_expected, 'html5lib')
